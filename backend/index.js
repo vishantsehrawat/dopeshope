07c162c5d1d1556a7f2a2fd658ec300a5369f7e0
vishant <vishant.96.sehrawat@gmail.com>
2023-11-06 23:44:19 +0530

    added intial setup for gql

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 
+const { ApolloServer, gql } = require("apollo-server-express");
 const { userRouter } = require("./routes/user.routes");
 const { connection } = require("./config/dbConnection");
 const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "Hello, GraphQL!",
+  },
+};
+// ^ apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// ^ Mounting Apollo Server as middleware on your Express app
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 require("dotenv").config();
 app.use(express.json());
@@ -17,10 +40,12 @@ app.use("/user", userRouter);
 app.listen(process.env.PORT, async (req, res) => {
   try {
     await connection;
+    startApolloServer();
     console.log("Connected to Database");
   } catch (error) {
     console.log(error);
   }
 
   console.log(`Server is running at port ${process.env.PORT}`);
+  console.log(`Apollod server is running at port http://localhost:8080/graphql`);
 });
