5f804665413d114a9db9634e816f17e59eb4d047
vishant <vishant.96.sehrawat@gmail.com>
2023-11-15 23:51:31 +0530

    delete , track, calculate price controller methods added

--- backend/controller/order.controller.js
+++ backend/controller/order.controller.js
@@ -55,6 +55,72 @@ const updateOrderDetails = async (req, res) => {
   }
 };
 
+// Route to track order status
+const trackOrderStatus = async (req, res) => {
+  const orderId = req.params.orderId;
+  try {
+    const order = await OrderModel.findById(orderId);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    return res.status(200).json({ success: true, status: order.status });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, error: "Error fetching order status" });
+  }
+};
+
+// Route to calculate order total price
+const calculateOrderTotalPrice = async (req, res) => {
+  const orderId = req.params.orderId;
+  try {
+    const order = await OrderModel.findById(orderId);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    const totalPrice = order.products.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+    return res.status(200).json({ success: true, totalPrice });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, error: "Error calculating total price" });
+  }
+};
+
+const handleOrderPayments = async (req, res) => {
+  const orderId = req.params.orderId;
+  return res.status(501).json({
+    success: false,
+    message: "Payment handling logic not implemented yet",
+  });
+};
+
+const deleteOrder = async (req, res) => {
+  const orderId = req.params.orderId;
+  try {
+    const order = await OrderModel.findByIdAndDelete(orderId);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: "Order deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, error: "Error deleting order" });
+  }
+};
+
 module.exports = {
   getOrderById,
   getOrdersByUser,
