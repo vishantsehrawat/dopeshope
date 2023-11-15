bfc9b90a6d4efe10d017be61074660135ec36e80
vishant <vishant.96.sehrawat@gmail.com>
2023-11-15 23:52:30 +0530

    added a few more controller method , left with detailed testing of all the routes

--- backend/controller/order.controller.js
+++ backend/controller/order.controller.js
@@ -121,6 +121,134 @@ const deleteOrder = async (req, res) => {
   }
 };
 
+// Route to place a new order
+const placeNewOrder = async (req, res) => {
+  try {
+    const orderData = req.body; 
+    const newOrder = new OrderModel(orderData);
+    await newOrder.save();
+    return res.status(201).json({
+      success: true,
+      message: "Order placed successfully",
+      order: newOrder,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while placing the order",
+    });
+  }
+};
+
+// Route to get orders
+const getOrders = async (req, res) => {
+  try {
+    const orders = await OrderModel.find();
+    return res.status(200).json({ success: true, orders });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while fetching orders",
+    });
+  }
+};
+
+// Route to cancel an order
+const cancelOrder = async (req, res) => {
+  const orderId = req.params.ID;
+  try {
+    const order = await OrderModel.findById(orderId);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    order.status = "Cancelled";
+    await order.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Order cancelled successfully", order });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while cancelling the order",
+    });
+  }
+};
+
+// Admin accessible route to get all orders
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await OrderModel.find();
+    return res.status(200).json({ success: true, orders });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while fetching all orders",
+    });
+  }
+};
+
+// Admin accessible route to change order status
+const changeOrderStatus = async (req, res) => {
+  const orderId = req.params.id;
+  const { newStatus } = req.body;
+  try {
+    const order = await OrderModel.findById(orderId);
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+    order.status = newStatus;
+    await order.save();
+    return res.status(200).json({
+      success: true,
+      message: "Order status updated successfully",
+      order,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while updating order status",
+    });
+  }
+};
+// Route to add products to an order
+const addProductsToOrder = async (req, res) => {
+  const { orderId } = req.params;
+  const { products } = req.body;
+
+  try {
+    const order = await OrderModel.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ success: false, error: "Order not found" });
+    }
+
+    order.products.push(...products);
+
+    const totalPrice = order.products.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+
+    order.totalPrice = totalPrice;
+
+    await order.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Products added to order successfully",
+      order,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "An error occurred while adding products to the order",
+    });
+  }
+};
+
 module.exports = {
   getOrderById,
   getOrdersByUser,
